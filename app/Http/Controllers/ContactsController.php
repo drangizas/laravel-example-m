0bf292f6ea660bff911ef3d2c9f31c706448750e
Zydrunas <dranginis.z@gmail.com>
2024-08-29 11:07:13 +0300

    Example with array as prop

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -8,6 +8,12 @@ class ContactsController extends Controller
 {
     function index(): View
     {
-        return view('contacts/index', ['title' => __('Text used as an example')]);
+        return view('contacts/index', [
+            'title' => [
+                __('Text used as an example'),
+                'Another Text used as an example',
+                'Third Text used as an example',
+            ]
+        ]);
     }
 }
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,8 +1,10 @@
 <x-base-layout>
     @if (App::getLocale() === 'lt')
-        <x-main-header :title="$title"/>
-        <x-main-header :title="$title"/>
+        @foreach ($title as $single_title)
+           <x-main-header :title="$single_title" />
+           <hr />
+        @endforeach
     @else
-        <x-main-header :title="$title"/>
+        <x-main-header :title="$title[0]"/>
     @endif
 </x-base-layout>
